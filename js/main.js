cbbbb9e75d770710b6031fe02fe7df732b733397
Abanoub-Louka <abanoub.louka94@gmail.com>
2024-06-04 20:51:21 +0300

    adding files

---
+++ js/main.js
@@ -0,0 +1,58 @@
+function Hospital(name, doctors, patients) {
+    this.name = name;
+    this.doctors = doctors;
+    this.patients = patients;
+
+    // add patient
+    this.addPatients = function addPatients (id,name) {
+        return this.patients[`patient_${id}`] = {
+            name,
+            diseases : {}
+        }
+    };
+
+    //add diseases
+    this.addDiseases = function addDiseases(patient_id, disease_id, name) {
+        return this.patients[`patient_${patient_id}`].diseases[`disease_${disease_id}`] = {
+            name
+        }
+    };
+
+    //remove disease from a patient 
+    this.removeDisease = function removeDisease(patient_id, disease_id) { 
+        delete this.patients[`patient_${patient_id}`].diseases[`disease_${disease_id}`]
+    };
+
+    //remove patient 
+    this.removePatient = function removePatient(patient_id) {
+        delete this.patients[`patient_${patient_id}`]
+    };
+
+    //add doctor 
+    this.addDoctors = function addDoctors (id,name) {
+        return this.doctors[`doctor_${id}`] = {
+            name
+        }
+    };
+
+    //remove doctor
+    this.removeDoctor = function removeDoctor(doctor_id) {
+        delete this.doctors[`doctor_${doctor_id}`]
+    };
+
+    //update doctor name 
+    this.updateDoctorName = function updateDoctorName(doctor_id, name) {
+        return this.doctors[`doctor_${doctor_id}`] = {
+            name
+        }
+    };
+
+    //assign doctor to patient  ??
+    this.assignDoctorToPatient = function assignDoctorToPatient(patient_id, doctor_id) {
+        return this.patients[`patient_${patient_id}`] = this.doctors[`doctor${doctor_id}`]
+    };
+
+    //get the doctor of specific patient ??
+};
+
+let hospital = new Hospital('new hospital', doctors = {} , patients = {});
